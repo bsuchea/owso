172c8405cbf20da25ced17bec68970927d62ef86
ouchi <bsuchea@gmail.com>
2021-01-16 23:31:57 +0700

    change something in dashboad

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
                     </div>
 
                     <div class="mt-8 text-2xl">
-                        Welcome to your Jetstream application!
+                        Welcome {{ auth()->user()->name }} !
                     </div>
 
                     <div class="mt-6 text-gray-500">
